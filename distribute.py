abad27a115dbc36ad37d8737f53484bfe769e8f7
cnexcale <36278502+cnexcale@users.noreply.github.com>
2022-04-06 09:13:33 +0200

    added new base command to distribute.py script

--- distribute.py
+++ distribute.py
@@ -5,8 +5,20 @@ import sys
 import subprocess
 
 
+
 # Constants for the script
 
+ignite_purge_flag = "purge"
+ignite_daemon_flag = "daemon"
+
+distribute_script = "./distribute-nextflow.sh"
+
+command_dist = "live"
+command_dry_run = "dry-run"
+
+# Argument defaults
+
+default_command = command_dry_run
 default_nf_target_dir = "/home/ubuntu/nf-ignite"
 default_setup_script = "./setup-nextflow.sh"
 default_user = "ubuntu"
@@ -14,15 +26,19 @@ default_daemon_flag = False
 default_ignite_discovery_dir = "/vol/spool/nf-ignite-cluster"
 default_purge_flag = False
 
-ignite_purge_flag = "purge"
-ignite_daemon_flag = "daemon"
 
-distribute_script = "./distribute-nextflow.sh"
 
 
 # Setup argparse arguments
 
 parser = argparse.ArgumentParser(description="Helper script to setup nextflow from source on a range of remote hosts.\n")
+
+parser.add_argument("command",
+                    help="Mode of action for this script. "
+                          + f"{command_dist} := will actually setup nextflow on hosts. "
+                          + f"{command_dry_run} := will only print generated command for the distribute script",
+                    type=str,
+                    choices=[command_dist, command_dry_run])
 
 parser.add_argument("nf_source",
                     metavar="nf-source",
@@ -54,19 +70,18 @@ parser.add_argument("--ignite-discovery", "-i",
                     default=default_ignite_discovery_dir)
 
 parser.add_argument("--daemon", "-d",
-                    help="If specified a nextflow ignite daemon will be launched on remote host after setup",
+                    help="A nextflow ignite daemon will be launched on remote host after setup",
                     action="store_true",
                     default=default_daemon_flag)
 
 parser.add_argument("--purge", "-p",
-                    help="If specified the existing nextflow installation on a remote host will be deleted before running the setup",
+                    help="The existing nextflow installation on a remote host will be deleted before running the setup",
                     action="store_true",
                     default=default_purge_flag)
 
 # Parse and validate commands
 
 args = parser.parse_args()
-print(args)
 
 # TODO - validate script (file exists), nf-source (dir exists), nf-target (absolute path) and ignite discovery (dir exists)
 
@@ -78,22 +93,28 @@ purge_param = ignite_purge_flag if args.purge else "no-purge"
 
 cmd = [distribute_script, args.nf_source, args.nf_target, args.setup_script, args.user, daemon_param, args.ignite_discovery, purge_param]
 
-print("Will execute:", " ".join(cmd))
 
+# Check command mode
 
-# Finally execute and capture stdout (redirect stderr to stdout)
+if args.command == command_dry_run:
 
-process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-# print("setting up process stdout capture")
-# proc_stdout, proc_stderr = process.communicate()
-# print("listen for streamed stdout")
+    print(f"\nDry run specified, built the following command. You can directly use this command with {distribute_script}")
+    print("\n  ", " ".join(cmd), "\n")
 
-while True:
-    out = process.stdout.readline()
+    sys.exit(0)
 
-    if not out and process.poll() is not None:
-        break
+if args.command == command_dist:
+    
+    # use redirected stderr for simplicity
 
-    print(out.decode("utf8").strip())
+    process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        out = process.stdout.readline()
+
+        if not out and process.poll() is not None:
+            break
+
+        print(out.decode("utf8").strip())
     
 
