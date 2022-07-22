6b0023992deaffa66ca1169fab88d57587288907
cnexcale <36278502+cnexcale@users.noreply.github.com>
2022-07-22 14:42:44 +0200

    parametrized local setup script

--- setup-locally.sh
+++ setup-locally.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 PARAM_NF_SOURCE="$1"
+DEFAULT_SOURCE="~/nf-current"
 
+SOURCE=""
 if [ "$PARAM_NF_SOURCE" == "" ]; then
-  echo "[-] nextflow source dir is missing!"
-  exit 1
+  echo "[~] nextflow source dir is missing, using default $DEFAULT_SOURCE"
+  SOURCE="$DEFAULT_SOURCE"
+else
+  SOURCE="$1"
 fi
 
-./setup-nextflow.sh ~/nf-current/nextflow
+./setup-nextflow.sh "$SOURCE"
